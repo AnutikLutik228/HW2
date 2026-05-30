932b63531f7643bcf88446517118342ed5d85ac0
Anna Orlova <annaandrorlova@edu.hse.ru>
2026-05-30 22:03:34 +0300

    Add ShoppingList class

--- main.py
+++ main.py
@@ -64,3 +64,63 @@ class Recipe:
     for ingredient in self.ingredients:
       result = result + str(ingredient) + "\n"
     return result
+
+class ShoppingList:
+  def __init__(self):
+    self._items = []
+
+  def add_recipe(self, recipe, portions):
+    if portions <= 0:
+      raise ValueError("Количество порций должно быть положительным")
+
+    scaled_recipe = recipe.scale(portions)
+
+    for ingredient in scaled_recipe.ingredients:
+      self._items.append((ingredient, recipe.title))
+
+  def remove_recipe(self, title):
+    new_items = []
+
+    for item in self._items:
+      if item[1] != title:
+        new_items.append(item)
+
+    self._items = new_items
+
+  def get_list(self):
+    products = {}
+
+    for item in self._items:
+      ingredient = item[0]
+
+      key = (ingredient.name, ingredient.unit)
+
+      if key in products:
+        products[key] += ingredient.quantity
+      else:
+        products[key] = ingredient.quantity
+
+    result = []
+
+    for key in products:
+      name = key[0]
+      unit = key[1]
+      quantity = products[key]
+
+      result.append(Ingredient(name, quantity, unit))
+
+    result.sort(key=lambda x: x.name)
+
+    return result
+
+  def __add__(self, other):
+    new_shopping_list = ShoppingList()
+
+    for item in self._items:
+      new_shopping_list._items.append(item)
+
+    for item in other._items:
+      new_shopping_list._items.append(item)
+
+    return new_shopping_list
+
