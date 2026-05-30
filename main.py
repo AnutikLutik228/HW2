bfd5cd1508309b552bf5cf4610e45ed23c16d561
Anna Orlova <annaandrorlova@edu.hse.ru>
2026-05-30 22:14:07 +0300

    Add DietaryRecipe class

--- main.py
+++ main.py
@@ -124,3 +124,16 @@ class ShoppingList:
 
     return new_shopping_list
 
+
+class DietaryRecipe(Recipe):
+  def __init__(self, title, diet_type, ingredients):
+    super().__init__(title, ingredients)
+    self.diet_type = diet_type
+
+  def scale(self, ratio):
+    new_recipe = super().scale(ratio)
+
+    return DietaryRecipe(new_recipe.title, self.diet_type, new_recipe.ingredients)
+
+  def __str__(self):
+    return f"[{self.diet_type}] {self.title}"
